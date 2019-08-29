91260f0d35b4d8027b254e9ef4dbf30ab9d6fc49
BrandedNomad <info@brandednomad.com>
2019-08-29 18:55:19 +0700

    Updates

---
+++ precache-manifest.461e386d36965b40d8444323325844ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e2a5135a7dde07fb2823ca9b40c1169",
+    "url": "/RecipeFinder/index.html"
+  },
+  {
+    "revision": "3e5b95716ec303423e50",
+    "url": "/RecipeFinder/static/css/main.3b5076da.chunk.css"
+  },
+  {
+    "revision": "5d8e61eeba18d69e3b26",
+    "url": "/RecipeFinder/static/js/2.7c484b41.chunk.js"
+  },
+  {
+    "revision": "3e5b95716ec303423e50",
+    "url": "/RecipeFinder/static/js/main.85d0d4e0.chunk.js"
+  },
+  {
+    "revision": "db71514d2bd82e93b02e",
+    "url": "/RecipeFinder/static/js/runtime~main.0635448e.js"
+  },
+  {
+    "revision": "3acd5a1c5e0d9f8b00a85ef93942b649",
+    "url": "/RecipeFinder/static/media/cuttingbg.3acd5a1c.jpg"
+  }
+]);
